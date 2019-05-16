60e6e4bee34bd7bbe0b5b6c12d650fe984ab9a64
noam-dan <49761325+noam-dan@users.noreply.github.com>
2019-05-16 19:59:31 +0300

    Update gentree.h

--- gentree.h
+++ gentree.h
@@ -1,1 +1,23 @@
+#ifndef TREE_H
+#define TREE_H
 
+#include "defs.h"
+
+typedef struct _tree* pTree;
+
+typedef void* pNode;
+
+typedef int (*GetKeyFunction)(pNode e);
+typedef pNode (*CloneFunction)(pNode e);
+typedef void (*PrintFunction)(pNode e);
+typedef void (*DelFunction)(pNode e);
+
+pTree TreeCreate(GetKeyFunction, CloneFunction, PrintFunction, DelFunction, int k);
+void TreeDestroy(pTree);
+int TreeNodesCount(pTree);
+void TreePrint(pTree);
+void TreePrint(pTree);
+Result TreeAddLeaf(pTree, PELEMENT, int key);
+
+
+#endif
